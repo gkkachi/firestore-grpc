f41ab9c961cd9691a43613b11dafd5fb391b22dd
gkkachi <gkkachi@users.noreply.github.com>
2022-04-03 01:07:43 +0000

    Deploying to gh-pages from  @ b5d69dccaa9c9dfef088196ab75c1ae4b7d9399f 🚀

--- tokio_util/sidebar-items.js
+++ tokio_util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["either","Module defining an Either type."],["sync","Synchronization primitives"]]});
+initSidebarItems({"mod":[["codec","Adaptors from AsyncRead/AsyncWrite to Stream/Sink"],["either","Module defining an Either type."],["sync","Synchronization primitives"]]});
