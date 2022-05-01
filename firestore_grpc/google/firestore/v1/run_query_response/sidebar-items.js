38d9bc9afb0f8793e0375a96029e87b4270a0517
gkkachi <gkkachi@users.noreply.github.com>
2022-05-01 01:32:53 +0000

    Deploying to gh-pages from  @ 6f7ee4fa43f98bf87580dafb57775ea36e05867d 🚀

---
+++ firestore_grpc/google/firestore/v1/run_query_response/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ContinuationSelector","The continuation mode for the query. If present, it indicates the current query response stream has finished. This can be set with or without a `document` present, but when set, no more results are returned."]]});
