c085a7946ac96e2d171bc101d5bb09016efcaee5
gkkachi <gkkachi@users.noreply.github.com>
2024-06-16 00:16:42 +0000

    Deploying to gh-pages from  @ 0a5ef22703507f4f83adfd8c5de5f6e5602bbf23 🚀

---
+++ search.desc/rustls_native_certs/rustls_native_certs-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("rustls_native_certs", 0, "rustls-native-certs allows rustls to use the platform’s …\nContains the error value\nContains the success value\nLike <code>Result&lt;T,E&gt;</code>, but allows for functions that can return …\nLoads root certificates found in the platform’s native …\nLoads root certificates found in the platform’s native …")
