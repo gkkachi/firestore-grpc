a86a7353a26d904e008d5ac1c3c14c92f07ee66c
gkkachi <gkkachi@users.noreply.github.com>
2025-02-02 00:18:40 +0000

    Deploying to gh-pages from  @ ec611f1641b15c49ce9fd96ad3f884abda0e8409 🚀

--- trait.impl/core/fmt/trait.Write.js
+++ trait.impl/core/fmt/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
+    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
