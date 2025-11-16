68ee681f74c644382cd1bc2b847ce8cc39ac2051
gkkachi <gkkachi@users.noreply.github.com>
2025-11-16 00:22:25 +0000

    Deploying to gh-pages from  @ 1330664072f669c0763d2344ec7c5fa461d67b93 🚀

--- trait.impl/core/borrow/trait.BorrowMut.js
+++ trait.impl/core/borrow/trait.BorrowMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
+    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
